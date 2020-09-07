62ce670970739c26a77a9e532111993df1e5bdff
Alex Nguyen <alex.nguyen.3141@gmail.com>
2020-09-07 11:29:30 +0700

    Fixed merge conflict

--- src/Types.h
+++ src/Types.h
@@ -1,9 +1,22 @@
-//
-// Created by usefulmana on 7/9/20.
-//
 
-#ifndef A2_TYPES_H
-#define A2_TYPES_H
+#ifndef COSC_ASSIGN_TWO_TYPES
+#define COSC_ASSIGN_TWO_TYPES
+
 #define NUM_OF_PLAYERS 2
 
-#endif //A2_TYPES_H
+// A game lasts exactly 5 rounds
+#define MAX_GAME_ROUNDS 5
+
+// Mosaic
+#define MOSAIC_DIM 5
+#define MOSAIC_BOARD_SIZE (MOSAIC_DIM * MOSAIC_DIM)
+
+// Factory
+#define FACTORY_SIZE 4
+
+// Deduct Pile
+#define DEDUCT_ROW_SIZE 7
+
+
+#endif // COSC_ASSIGN_TWO_TYPES
+
