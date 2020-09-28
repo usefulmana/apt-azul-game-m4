abb9258ac780c60868fa5fde23664cca78c5f2a7
Mitchell Gust s3782095 <s3782095@student.rmit.edu.au>
2020-09-28 18:50:31 +1000

    Using Grid and character find

--- src/Score.cpp
+++ src/Score.cpp
@@ -1,35 +1,66 @@
 #include "Score.h"
+#define EMPTY_SPACE '.'
 
-Score::Score(Tile newlyPlaced, Player player) {   
+Score::Score(Tile ** newlyPlaced, Player player) {   
     // Declare Variables
     this->newlyPlaced = newlyPlaced;
     this->roundScore = 0;
     this->player = player;
+    this->grid = player.getGrid();
 
     // Calculate Scoring
-    horizontalScoring();
-    verticalScoring();
+    addScoring();
 
     // Add Round Score to Player Total
     player.addScore(roundScore);
 }
 
-void Score::horizontalScoring() {
-    current = newlyPlaced;
-    while (isRight(current)) {
-        roundScore++;
-        current = next;
+void Score::addScoring() {
+    // For up, down, left, right
+    for (int i = 0; i < 4; i ++){
+
+        current = newlyPlaced;
+        while (nextExists(current)) {
+            roundScore++;
+            current = next;
+        }
     }
 }
 
 
-bool Score::isRight(Tile current) {
-    if () { // Tile found next to current
-        next = ; // Position of Next Tile
+bool Score::nextExists(Tile ** current) {
+    int currentX = current.getX();
+    int currentY = current.getY();
+
+    getNextCoordinates(currentX, currentY);
+    
+    if (grid[nextX][nextY] != EMPTY_SPACE) { // Tile found next to current
+        next = grid[nextX][nextY]; // Position of Next Tile
         return true; 
     } else {
         return false; 
     }
+}
+
+void Score::getNextCoordinates(int currentX, int currentY) {
+
+    nextY = currentY;
+    nextX = currentX;
+
+    if (rightDone == false) {
+        // Right
+        nextX = currentX + 1;
+    } else if (leftDone == false) {
+        // Left
+        nextX = currentX - 1;
+    } else if (upDone == false) {
+        // Up
+        nextY = currentY + 1;
+    } else if (downDone == false) {
+        // Down
+        nextY = currentY - 1;
+    }
+
 }
 
 int Score::getRoundScore() {
--- src/Score.h
+++ src/Score.h
@@ -2,40 +2,37 @@
 #define COSC_ASSIGN_TWO_SCORE
 #include "Player.h"
 
-
 class Score
 {
 public:
-    Score(Tile newlyPlaced, Player player);
+    Score(Tile ** newlyPlaced, Player player);
 
     // Apply Horizontal Scoring
-    void horizontalScoring();
-    // Check if Tile on Right
-    bool isRight(Tile current);
-    // Check if Tile on Left
-    bool isLeft(Tile current);
-
-    // Apply Vertical Scoring
-    void verticalScoring();
-    // Check if Tile on Top
-    bool isTop(Tile current);
-    // Check if Tile on Bottom
-    bool isBottom(Tile current);
+    void addScoring();
+    // Check if there is a next tile
+    bool nextExists(Tile ** current);
+    // Decide to go up, down, left or right
+    void getNextCoordinates(int currentX, int currentY);
 
     // Get Round Score
     int getRoundScore();
 
 private:
     // Placed Piece in Mosaic
-    Tile newlyPlaced;
+    Tile ** newlyPlaced;
 
     // Checking From
-    Tile current;
+    Tile ** current;
     // Checking
-    Tile next;
+    Tile ** next;
+
+    // Next Position to check
+    int nextX;
+    int nextY;
 
     int roundScore;
     Player player;
+    Tile ** grid;
 };
 
 #endif // COSC_ASSIGN_TWO_SCORE
